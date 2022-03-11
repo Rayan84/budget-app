39e4a30afdc9e7bb301b14ac74549e1603cecda3
Rayan84 <rayan_84@live.com>
2022-03-11 19:01:52 +0100

    Create categories page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,7 +9,9 @@ class CategoriesController < ApplicationController
   end
 
   # GET /categories/1 or /categories/1.json
-  def show; end
+  def show;
+    @category = Category.find_by(id: params[:id])
+  end
 
   # GET /categories/new
   def new
